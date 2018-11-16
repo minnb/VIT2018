b05e22552938121a2ad9b68c138456916fd639bd
unknown <minhnb.it@gmail.com>
2018-11-16 22:34:50 +0700

    angular

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -1,2 +1,2 @@
-var app = angular.module('app-VIT',[]).constant('API', 'http://localhost:8000/project/vitduct/dashboard/');
+var app = angular.module('app-VIT',[]).constant('API', 'http://vitduct.com.vn/dashboard/');
 
