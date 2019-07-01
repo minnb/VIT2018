16e00080e85953a0d98d7bb334c3d0ab44fd1c09
minnb <minhnb.it@gmail.com>
2019-07-02 06:11:47 +0700

    s

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -1,7 +1,2 @@
 var app = angular.module('app-VIT',[]).constant('API', 'http://vitduct.com.vn/dashboard/');
-<<<<<<< HEAD
-
-=======
-//var app = angular.module('app-VIT',[]).constant('API', 'http://localhost:8000/project/vitduct/dashboard/');
->>>>>>> ebce1cbcb42253cf63a23657b3a938cc9a2caadd
 
